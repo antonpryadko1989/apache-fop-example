fd565b3ee1a5e26df14d237e4a73e0629c29c1a0
antonpryadko1989 <anton.pryadko.1989@gmail.com>
2018-06-19 11:26:34 +0300

    add method for convert object to xml_1

---
+++ src/main/java/com/visoft/api/TemplateController.java
@@ -0,0 +1,10 @@
+package com.visoft.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/visoft/api")
+public class TemplateController {
+
+}
